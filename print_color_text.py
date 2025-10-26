3cc6d32ea69e337cf57b8475e0c996b54ab5dd93
Moose <mowinckel107@gmail.com>
2025-10-26 15:38:31 +0100

    Initial commit

---
+++ print_color_text.py
@@ -0,0 +1,25 @@
+
+from typing import Optional
+
+WHITE_ANSI_COLOR_CODE = 37
+
+def turn_text_colur( ansi_color_escape_code : int):
+    print(f"\033[{ansi_color_escape_code}m", end="")
+
+def print_text_red(text : str, print_end : Optional[str] = "\n"):
+    red_ansi_color_code : int = 31
+    turn_text_colur(red_ansi_color_code)
+    print(text, end=print_end)
+    turn_text_colur(WHITE_ANSI_COLOR_CODE)
+
+def print_text_green(text : str, print_end : Optional[str] = "\n"):
+    red_ansi_color_code : int = 32
+    turn_text_colur(red_ansi_color_code)
+    print(text, end=print_end)
+    turn_text_colur(WHITE_ANSI_COLOR_CODE)
+
+def print_text_yellow(text : str, print_end : Optional[str] = "\n"):
+    red_ansi_color_code : int = 33
+    turn_text_colur(red_ansi_color_code)
+    print(text, end=print_end)
+    turn_text_colur(WHITE_ANSI_COLOR_CODE)

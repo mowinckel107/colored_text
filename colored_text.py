828ce4b0960d7506ff887d899b8b19be8270db1a
Moose <mowinckel107@gmail.com>
2025-11-23 19:07:29 +0100

    fixed errors

--- colored_text.py
+++ colored_text.py
@@ -10,36 +10,36 @@ def __turn_text_colur( ansi_color_escape_code : int):
 
 
 def print_text_red(text : str, print_end : Optional[str] = "\n"):
-    red_ansi_color_code : int = 31 # normal red
+    ansi_color_code : int = 31 # normal red
 
-    __turn_text_colur(red_ansi_color_code)
+    __turn_text_colur(ansi_color_code)
     print(text, end=print_end)
     __turn_text_colur(WHITE_ANSI_COLOR_CODE)
 
 
 
 def print_text_green(text : str, print_end : Optional[str] = "\n"):
-    red_ansi_color_code : int = 92 # bright green
+    ansi_color_code : int = 92 # bright green
 
-    __turn_text_colur(red_ansi_color_code)
+    __turn_text_colur(ansi_color_code)
     print(text, end=print_end)
     __turn_text_colur(WHITE_ANSI_COLOR_CODE)
 
 
 
 def print_text_yellow(text : str, print_end : Optional[str] = "\n"):
-    red_ansi_color_code : int = 93 # bright yellow
+    ansi_color_code : int = 93 # bright yellow
 
-    __turn_text_colur(red_ansi_color_code)
+    __turn_text_colur(ansi_color_code)
     print(text, end=print_end)
     __turn_text_colur(WHITE_ANSI_COLOR_CODE)
 
 
 
 def print_text_blue(text : str, print_end : Optional[str] = "\n"):
-    red_ansi_color_code : int = 94 # bright blue
+    ansi_color_code : int = 94 # bright blue
 
-    __turn_text_colur(red_ansi_color_code)
+    __turn_text_colur(ansi_color_code)
     print(text, end=print_end)
     __turn_text_colur(WHITE_ANSI_COLOR_CODE)
 
@@ -55,9 +55,9 @@ def __text_colur( ansi_color_escape_code : int) -> str:
 
 
 def red_text(text : str) -> str:
-    red_ansi_color_code : int = 31 # normal red
+    ansi_color_code : int = 31 # normal red
 
-    colored_text : str = __text_colur(red_ansi_color_code)
+    colored_text : str = __text_colur(ansi_color_code)
     colored_text      += text
     colored_text      += __text_colur(WHITE_ANSI_COLOR_CODE)
 
@@ -66,9 +66,9 @@ def red_text(text : str) -> str:
 
 
 def green_text(text : str) -> str:
-    red_ansi_color_code : int = 92 # bright green
+    ansi_color_code : int = 92 # bright green
 
-    colored_text : str = __text_colur(red_ansi_color_code)
+    colored_text : str = __text_colur(ansi_color_code)
     colored_text      += text
     colored_text      += __text_colur(WHITE_ANSI_COLOR_CODE)
 
@@ -77,9 +77,9 @@ def green_text(text : str) -> str:
 
 
 def yellow_text(text : str) -> str:
-    red_ansi_color_code : int = 93 # bright yellow
+    ansi_color_code : int = 93 # bright yellow
 
-    colored_text : str = __text_colur(red_ansi_color_code)
+    colored_text : str = __text_colur(ansi_color_code)
     colored_text      += text
     colored_text      += __text_colur(WHITE_ANSI_COLOR_CODE)
     
@@ -88,9 +88,9 @@ def yellow_text(text : str) -> str:
 
 
 def blue_text(text : str) -> str:
-    red_ansi_color_code : int = Bright Blue # bright blue
+    ansi_color_code : int = 94 # bright blue
 
-    colored_text : str = __text_colur(red_ansi_color_code)
+    colored_text : str = __text_colur(ansi_color_code)
     colored_text      += text
     colored_text      += __text_colur(WHITE_ANSI_COLOR_CODE)
     
